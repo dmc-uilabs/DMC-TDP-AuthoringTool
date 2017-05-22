71cc2f1f4cddff1597e2b8b9b818fa30d8e08ecc
Jonathan Brand <jhbrand@umich.edu>
2017-05-22 17:28:35 -0500

    changed dependencies

--- generateTDP.py
+++ generateTDP.py
@@ -1,11 +1,160 @@
 #!/usr/bin/python
 # coding: utf-8
 
-from STParser import stp_header_parser
 import aocxchange.step
-from OCCUtils import Common
 import xml.etree.cElementTree as ET
 import uuid
+import re
+from OCC.Bnd import Bnd_Box
+from OCC.BRepBndLib import brepbndlib_Add
+from OCC.GProp import GProp_GProps
+from OCC.BRepGProp import (brepgprop_LinearProperties,
+                           brepgprop_SurfaceProperties,
+                           brepgprop_VolumeProperties)
+
+TOLERANCE = 1e-6
+
+class GpropsFromShape(object):
+    def __init__(self, shape, tolerance=1e-5):
+        self.shape = shape
+        self.tolerance = tolerance
+
+    def volume(self):
+        '''returns the volume of a solid
+        '''
+        prop = GProp_GProps()
+        brepgprop_VolumeProperties(self.shape, prop, self.tolerance)
+        return prop
+
+    def surface(self):
+        '''returns the area of a surface
+        '''
+        prop = GProp_GProps()
+        brepgprop_SurfaceProperties(self.shape, prop, self.tolerance)
+        return prop
+
+    def linear(self):
+        '''returns the length of a wire or edge
+        '''
+        prop = GProp_GProps()
+        brepgprop_LinearProperties(self.shape, prop)
+        return prop
+
+def get_boundingbox(shape, tol=TOLERANCE):
+    '''
+    :param shape: TopoDS_Shape such as TopoDS_Face
+    :param tol: tolerance
+    :return: xmin, ymin, zmin, xmax, ymax, zmax
+    '''
+    bbox = Bnd_Box()
+    bbox.SetGap(tol)
+    brepbndlib_Add(shape, bbox)
+    xmin, ymin, zmin, xmax, ymax, zmax = bbox.Get()
+    return xmin, ymin, zmin, xmax, ymax, zmax
+
+class stp_header_parser():
+
+    def stp_header_parser(self, stp_filename='', is_debug=False):
+
+        def get_unit_abbr(units):
+            prefix = {'MILLI': 'm'}
+            unit = {'METRE': 'm'}
+            return prefix[units[0]]+unit[units[1]]
+            
+        def remove_comments(line):
+            comment_pattern = re.compile('/\*.*?\*/')
+            return comment_pattern.sub('', line)
+        
+        def line_extract(filehandle=None, str_startswith='', str_endswith=''):
+
+            while True:
+
+                line = filehandle.readline().strip()
+                #if line.startswith(str_startswith):
+                if str_startswith in line:
+                    line_extracted = ''
+                    while True:
+                        line_extracted += line
+                        if line.endswith(str_endswith):
+                            break
+                        else:
+                            line = filehandle.readline().strip()
+
+                    return line_extracted
+
+        infos_name = [
+            'ISO Standard',
+            'Description',
+            'Implementation Level',
+            'Name',
+            'Time_Stamp',
+            'Author',
+            'Organization',
+            'Preprocessor Version',
+            'Originating System',
+            'Authorization',
+            'Schema',
+            'Unit'
+        ]
+
+        infos_value = []
+
+        len_infos_name = len(infos_name)
+
+        with open(stp_filename, 'r') as f:
+            
+            line = line_extract(f, 'ISO-', ';')
+
+            if line:
+                ISO_Standard = line[:-1]
+                infos_value.append(ISO_Standard)
+
+            line = line_extract(f, 'HEADER', ';')
+
+            if line:
+                if is_debug:
+                    print('>>> Header Start Mark Found <<<')
+
+                line = line_extract(f, 'FILE_DESCRIPTION', ';')
+                line = remove_comments(line)
+                File_Description = eval(line[16:-1])
+                infos_value += File_Description
+
+                line = line_extract(f, 'FILE_NAME', ';')
+                line = remove_comments(line)
+                File_Name = eval(line[9:-1])
+                infos_value += File_Name
+
+                line = line_extract(f, 'FILE_SCHEMA', ';')
+                line = remove_comments(line)
+                File_Schema = eval(line[11:-1])
+                infos_value.append(File_Schema)
+
+                if line_extract(f, 'ENDSEC', ';'):
+                    if is_debug:
+                        print('>>> Header End Mark Found <<<')
+            
+            while True:
+                line = line_extract(f, 'LENGTH_UNIT', ';')
+                units = line.split('SI_UNIT')
+                if(len(units) == 2):
+                    units = units[1].split('.')[1:4:2]
+                    units = get_unit_abbr(units)
+                    infos_value.append(units)
+                    break
+                
+
+        infos_dict = {
+            index: list(parameter) for (
+                index, parameter) in zip(
+                range(len_infos_name), zip(
+                    infos_name, infos_value))}
+
+        if is_debug:
+            for key in infos_dict:
+                print('{:02}\t{}'.format(key, infos_dict[key]))
+
+        return infos_dict
 
 def get_dome_inputs():
     with open('in.txt') as f:
@@ -22,7 +171,7 @@ def get_dome_inputs():
     return inputs
     
 def get_metadata(filename, material, coatings):
-    header = stp_header_parser.stp_header_parser()
+    header = stp_header_parser()
     header = header.stp_header_parser(stp_filename=filename)
     
     return {'name': header[3][1], 'material': material, 'coatings': coatings, 'unit': header[11][1]}
@@ -32,12 +181,12 @@ def import_step(filename):
     return my_importer.shapes[0]
     
 def get_geometry(shape, unit="units"):
-    boundingbox_points = Common.get_boundingbox(shape)
+    boundingbox_points = get_boundingbox(shape)
     length = boundingbox_points[3] - boundingbox_points[0]
     height = boundingbox_points[5] - boundingbox_points[2]
     width = boundingbox_points[4] - boundingbox_points[1]
     
-    gprop = Common.GpropsFromShape(shape)
+    gprop = GpropsFromShape(shape)
     volume = gprop.volume().Mass()
     density = 1
     mass = volume*density
