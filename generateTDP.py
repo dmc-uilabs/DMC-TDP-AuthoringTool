0941b1384efb406a537545b5bb4abf40e33895d7
Jonathan Brand <jhbrand@umich.edu>
2017-05-24 11:03:29 -0500

    update with zip file generate

--- generateTDP.py
+++ generateTDP.py
@@ -1,11 +1,17 @@
 #!/usr/bin/python
 # coding: utf-8
 
-import aocxchange.step
-import xml.etree.cElementTree as ET
 import uuid
 import re
+import time
+import sys
+import zipfile
+import aocxchange.step
+import xml.etree.cElementTree as ET
+from PyQt5 import QtWidgets
+from PIL import Image
 from OCC.Bnd import Bnd_Box
+from OCC.Display.OCCViewer import Viewer3d
 from OCC.BRepBndLib import brepbndlib_Add
 from OCC.GProp import GProp_GProps
 from OCC.BRepGProp import (brepgprop_LinearProperties,
@@ -13,6 +19,21 @@ from OCC.BRepGProp import (brepgprop_LinearProperties,
                            brepgprop_VolumeProperties)
 
 TOLERANCE = 1e-6
+
+# Unit: kg/m^3
+DENSITIES = {
+    "Steel": 8000,
+    "Aluminum": 2700
+}
+
+UNIT_FACTOR = {
+    "units": 1,
+    "m": 1,
+    "cm": .01,
+    "mm": .001
+}
+
+VIEWS = ["front", "rear", "top", "bottom", "left", "right", "iso"]
 
 class GpropsFromShape(object):
     def __init__(self, shape, tolerance=1e-5):
@@ -164,8 +185,8 @@ def get_dome_inputs():
 
     for line in lines:
         kv = line.rstrip().split("=")
-        key = kv[0]
-        value = kv[1]
+        key = kv[0].strip()
+        value = kv[1].strip()
         inputs[key] = value
         
     return inputs
@@ -180,7 +201,7 @@ def import_step(filename):
     my_importer = aocxchange.step.StepImporter(filename)
     return my_importer.shapes[0]
     
-def get_geometry(shape, unit="units"):
+def get_geometry(shape, material, unit="units"):
     boundingbox_points = get_boundingbox(shape)
     length = boundingbox_points[3] - boundingbox_points[0]
     height = boundingbox_points[5] - boundingbox_points[2]
@@ -188,8 +209,8 @@ def get_geometry(shape, unit="units"):
     
     gprop = GpropsFromShape(shape)
     volume = gprop.volume().Mass()
-    density = 1
-    mass = volume*density
+    density = DENSITIES[material]
+    mass = volume*density*pow(UNIT_FACTOR[unit], 3)
     surface_area = gprop.surface().Mass()
     
     return {'length': length, 'height': height, 'width': width, 'volume': volume, 'mass': mass, 'surface_area': surface_area}
@@ -209,7 +230,56 @@ def generate_xml(metadata, geometry, part_id):
     ET.SubElement(manufacturingDetails, "material").text = metadata["material"]
     ET.SubElement(manufacturingDetails, "coatings").text = metadata["coatings"]
     
-    return ET.tostring(mBOM)
+    return mBOM
+
+def generate_snapshots(shape):
+    app = QtWidgets.QApplication(sys.argv)
+    widget = QtWidgets.QWidget()
+    widget.resize(1000,1000)
+    view = Viewer3d(int(widget.winId()))
+    view.Create()
+    view.SetModeShaded()
+    view.DisplayShape(shape, update=True)
+    
+    VIEW_FUNC = {
+        "front": view.View_Front,
+        "rear": view.View_Rear,
+        "top": view.View_Top,
+        "bottom": view.View_Bottom,
+        "left": view.View_Left,
+        "right": view.View_Right,
+        "iso": view.View_Iso
+    }
+    
+    snapshots = []
+    
+    for view_type in VIEWS:
+        VIEW_FUNC[view_type]()
+        view.ExportToImage('capture.ppm')
+        im = Image.open('capture.ppm')
+        snapshot = view_type + '_capture.png'
+        im.save(snapshot)
+        snapshots.append(snapshot)
+    
+    return snapshots
+
+def generate_zip(xml, filename, snapshots):
+    file_id = int(time.time())
+    zip_filename = 'TDP_' + str(file_id) + '.zip'
+    
+    tree = ET.ElementTree(xml)
+    xml_file = "TDP_" + str(file_id) + ".xml"
+    tree.write(xml_file)
+    
+    with zipfile.ZipFile(zip_filename, 'w') as myzip:
+        myzip.write(filename)
+        myzip.write(xml_file)
+        for snapshot in snapshots:
+            myzip.write(snapshot)
+        
+    myzip.close()
+    
+    return zip_filename
 
 if __name__ == '__main__':
     inputs = get_dome_inputs()
@@ -222,9 +292,14 @@ if __name__ == '__main__':
     metadata = get_metadata(filename, material, coatings)
     
     shape = import_step(filename)
-    geometry = get_geometry(shape)
+    geometry = get_geometry(shape, material, metadata["unit"])
     
     xml = generate_xml(metadata, geometry, part_id)
+    
+    snapshots = generate_snapshots(shape)
+    
+    zip_filename = generate_zip(xml, filename, snapshots)
+    
     outfile = open('out.txt', 'w')
-    outfile.write("TDP_xml=" + xml)
+    outfile.write("TDP_xml=" + zip_filename)
     outfile.close()
